5563bddb13157a4b7a1cb0f3948b4913b524b35e
abdulkhamidov <aabdulkhamidov818@gmail.com>
2024-08-31 22:11:05 +0500

    responsive nav bar js function

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const sitebar = document.querySelector(".sitebar__list");
+
+function showSidebar() {
+  sitebar.style.display = "flex";
+}
+function hideSidebar() {
+  sitebar.style.display = "none";
+}
